3fe97cd7c7c289d3ef95d6cd28581f5d224c0bd4
sebygaa <sebyga@gmail.com>
2025-01-05 14:47:57 +0900

    Ergun equation convergence

--- column_Pswing_v02.py
+++ column_Pswing_v02.py
@@ -32,38 +32,6 @@ dd = dd_upp - 2*dd_mid + dd_low
 d[0,:] = 0
 d[-1,:]=0
 
-# %%
-# Isotherm Model 
-# %%
-qm1 = 0.5
-qm2 = 3
-b1 = 0.01
-b2 = 0.2
-def iso(P1,P2):
-    deno = 1+b1*P1 + b2*P2
-    nume1 = qm1*b1*P1
-    nume2 = qm2*b2*P2
-    q1 = nume1/deno
-    q2 = nume2/deno
-    return q1, q2
-
-# %%
-# Initial Value
-# %%
-# Initial P, T, C1, C2, q1, q2, y0
-P_init = 1E5*np.ones([N,])
-T_init = 300
-R_gas = 8.3145
-C1_init = 1*P_init/R_gas/T_init*np.ones([N,])
-C2_init = 0*P_init/R_gas/T_init*np.ones([N,])
-q1_init, q2_init = iso(1*P_init, 0*P_init)
-y0 = np.concatenate([C1_init, C2_init, q1_init, q2_init])
-
-# Boundary conditions
-P_feed = 1E5
-C1_feed = 0.5*P_feed/R_gas/T_init
-C2_feed = 0.5*P_feed/R_gas/T_init
-
 # %% 
 # Ergun equation
 # %%
@@ -92,8 +60,8 @@ def Ergun(P,z,d_p, mu, rho_g,void_frac):
 # Test of Ergun eqn function
 dp_test = 1E-2  # 0.01 (m) = 1cm
 mu_test = 1.8E-5 # (Pa s)
-Mw1_test = 28
-Mw2_test = 44 
+Mw1_test = 28/1E3   # kg/mol
+Mw2_test = 44/1E3   # kg/mol
 C1_test = np.logspace(np.log10(10),np.log10(30),N)
 C2_test = np.zeros_like(C1_test)
 C2_test[:80] = np.linspace(50,5,80)
@@ -102,14 +70,15 @@ C1_mid = (C1_test[1:] + C1_test[:-1])/2
 C2_mid = (C2_test[1:] + C2_test[:-1])/2
 rho_g_test = C1_mid*Mw1_test + C2_mid*Mw2_test
 C_ov = C1_test + C2_test
-P_ov_test = C_ov*8.3145*300/1E5
+P_ov_test = C_ov*8.3145*300
 epsi_test = 0.3
 u_test,_,_= Ergun(P_ov_test,z_dom, dp_test,mu_test, rho_g_test,epsi_test)
 plt.figure(dpi=300)
 plt.plot(z_dom[1:]-0.5*z_dom[1], u_test, 'o')
-
+#plt.plot(z_dom, P_ov_test)
 # Ergun for binary system
-def Ergun_bin(C_list, T_gas, z, d_p, mu, Mw_list, void_frac):
+def Ergun_bi(C_list, T_gas, z,
+              d_p, mu, Mw_list, void_frac):
     C1, C2 = C_list
     Mw1,Mw2 = Mw_list
     C1_mid = (C1[1:] + C1[:-1])/2
@@ -120,9 +89,47 @@ def Ergun_bin(C_list, T_gas, z, d_p, mu, Mw_list, void_frac):
     u_ret, arg_u_pos, arg_u_neg = Ergun(P_ov_tmp, 
                                         z, d_p, mu, rho_g_tmp, void_frac)
     return u_ret, arg_u_pos, arg_u_neg
-    
-#plt.plot(z_dom, P_ov_test)
-#plt.ylim([-5.0, -5.1])
+
+
+# %%
+# Isotherm Model 
+# %%
+qm1 = 0.5
+qm2 = 3
+b1 = 0.01
+b2 = 0.2
+def iso(P1,P2):
+    deno = 1+b1*P1 + b2*P2
+    nume1 = qm1*b1*P1
+    nume2 = qm2*b2*P2
+    q1 = nume1/deno
+    q2 = nume2/deno
+    return q1, q2
+
+# %%
+# Initial Values
+# %%
+# Initial P, T, C1, C2, q1, q2, y0
+P_init = 1.5E5*np.ones([N,])    # (Pa)
+T_init = 300
+R_gas = 8.3145
+C1_init = 1*P_init/R_gas/T_init*np.ones([N,])
+C2_init = 0*P_init/R_gas/T_init*np.ones([N,])
+q1_init, q2_init = iso(1*P_init*1E-5, 0*P_init*1E-5)
+y0 = np.concatenate([C1_init, C2_init, q1_init, q2_init])
+
+# %%
+# Boundary Conditions
+# %%
+# Pressure conditions
+P_end = 1       # (bar)
+Cv_out = 1E-2 
+
+P_feed = 1.5    # (bar)
+# Inlet conditions
+T_feed = T_init
+C1_feed = 0.5*P_feed*1E5/R_gas/T_feed
+C2_feed = 0.5*P_feed*1E5/R_gas/T_feed
 
 # %%
 # Model for odeint
@@ -131,10 +138,15 @@ def Ergun_bin(C_list, T_gas, z, d_p, mu, Mw_list, void_frac):
 D_AB = 1E-6
 h = h_arr[0]
 T_g = T_init
-u_g = 0.01      # m/s : advective velocity
-rho_s = 1000    # kg/m^3
+#u_g = 0.01      # (m/s) : advective velocity
+u_feed = 0.00
+rho_s = 1000    # (kg/m^3)
 epsi = 0.35     # voide fraction of pellet
-k1, k2 = [0.3,0.3]
+k1, k2 = [0.2,0.2]
+d_particle = 2E-3   # (m) : pellet particle size
+mu_gas = 1.8E-5 # (Pa s) : viscosity of gas (air)  
+Mw_gas = np.array([44,28])*1E-3 # (kg/mol) : molar weight for each component
+
 
 # PDE -> ODE model
 def model_col(y,T ):
@@ -147,6 +159,17 @@ def model_col(y,T ):
     P1 = C1*R_gas*T_g/1E5   # in bar
     P2 = C2*R_gas*T_g/1E5   # in bar
     
+    # velocity from Ergun equation
+    u_g, arg_u_posi, arg_u_nega = Ergun_bi([C1,C2], T_g, z_dom,
+                                           d_particle, mu_gas,
+                                           Mw_gas, epsi)
+    u_g = np.concatenate([[0],u_g])
+    du_g = (u_g[1:] - u_g[:-1])/h
+    du_g = np.concatenate([du_g,[0]])
+
+    # Valve equation
+    P_ov_end = P1[-1]+P2[-1]
+    u_out = Cv_out*(P_ov_end - P_end) # (bar) -> m/s
     # Isotherm
     q1sta, q2sta = iso(P1,P2,)
 
@@ -161,33 +184,40 @@ def model_col(y,T ):
     dq2dt = k2*(q2sta - q2)
 
     # Mass balanace
-    dC1dt = D_AB*ddC1 - u_g*dC1 - rho_s*(1-epsi)/epsi*dq1dt
-    dC2dt = D_AB*ddC2 - u_g*dC2 - rho_s*(1-epsi)/epsi*dq2dt
+    dC1dt = D_AB*ddC1 - u_g*dC1 - du_g*C1 - rho_s*(1-epsi)/epsi*dq1dt
+    dC2dt = D_AB*ddC2 - u_g*dC2 - du_g*C2 - rho_s*(1-epsi)/epsi*dq2dt
 
     # Boundary conditions
-    dC1dt[0] = D_AB*(C1[1]-2*C1[0]+C1_feed)/h**2 - u_g*(C1[0] - C1_feed)/h - rho_s*(1-epsi)/epsi*dq1dt[0]
-    dC2dt[0] = D_AB*(C2[1]-2*C2[0]+C2_feed)/h**2 - u_g*(C2[0] - C2_feed)/h - rho_s*(1-epsi)/epsi*dq2dt[0]
-    dC1dt[-1] = D_AB*(C1[-2]-2*C1[-1]+C1[-2])/h**2 - u_g*(C1[-1] - C1[-2])/h - rho_s*(1-epsi)/epsi*dq1dt[-1]
-    dC2dt[-1] = D_AB*(C2[-2]-2*C2[-1]+C2[-2])/h**2 - u_g*(C2[-1] - C2[-2])/h - rho_s*(1-epsi)/epsi*dq2dt[-1]
+    #dC1dt[0] = D_AB*(C1[1]-2*C1[0]+C1_feed)/h**2 - (u_g[1]*C1[0] - u_feed*C1_feed)/h - rho_s*(1-epsi)/epsi*dq1dt[0]
+    #dC2dt[0] = D_AB*(C2[1]-2*C2[0]+C2_feed)/h**2 - (u_g[1]*C2[0] - u_feed*C2_feed)/h - rho_s*(1-epsi)/epsi*dq2dt[0]
+    #dC1dt[-1] = D_AB*(C1[-2]-2*C1[-1]+C1[-2])/h**2 - (u_out*C1[-1] - u_g[-2]*C1[-2])/h - rho_s*(1-epsi)/epsi*dq1dt[-1]
+    #dC2dt[-1] = D_AB*(C2[-2]-2*C2[-1]+C2[-2])/h**2 - (u_out*C2[-1] - u_g[-2]*C2[-2])/h - rho_s*(1-epsi)/epsi*dq2dt[-1]
+    
+    dC1dt[0] = - (u_g[1]*C1[0] - u_feed*C1_feed)/h - rho_s*(1-epsi)/epsi*dq1dt[0]
+    dC2dt[0] = - (u_g[1]*C2[0] - u_feed*C2_feed)/h - rho_s*(1-epsi)/epsi*dq2dt[0]
+
+    dC1dt[-1] = - (u_out*C1[-1] - u_g[-1]*C1[-2])/h - rho_s*(1-epsi)/epsi*dq1dt[-1]
+    dC2dt[-1] = - (u_out*C2[-1] - u_g[-1]*C2[-2])/h - rho_s*(1-epsi)/epsi*dq2dt[-1]
+    
     dydt = np.concatenate([dC1dt,dC2dt, dq1dt, dq2dt])
     return dydt
 
 # %%
 # Run 
 # %%
-t_ran = np.arange(0,300+0.1,0.1)
+t_ran = np.arange(0,10+0.05,0.05)
 y_res = odeint(model_col, y0, t_ran )
 print(y_res.shape)
 # %%
 # Graph drawing
 # %%
-t_sample = t_ran[::200]
-ii_arr = np.arange(len(t_ran))[::200]
+t_sample = t_ran[::20]
+ii_arr = np.arange(len(t_ran))[::20]
 
 ls_list = ['-','--','-.',':']
 cc = 0
 for ii, tt in zip(ii_arr, t_sample):
-    C_samp = y_res[ii, N:2*N]
+    C_samp = y_res[ii, 0*N:1*N]
     plt.plot(z_dom, C_samp, 'k',
              linestyle = ls_list[cc%len(ls_list)],
              label = f't = {tt}'
